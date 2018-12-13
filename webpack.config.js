5b7fc9501290f5213335e2d62b226813ad3d3dd1
Brian Emilius <brian@brianemilius.com>
2018-12-13 08:41:30 +0100

    add basic project setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// webpack v4
+
+const path = require('path');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'zero.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
