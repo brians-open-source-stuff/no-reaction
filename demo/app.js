0483f53d2dd8ea8de7ca20f0c7171efac7eca2a5
Brian Emilius <brian@brianemilius.com>
2018-12-16 18:16:52 +0100

    add event to demo

--- demo/app.js
+++ demo/app.js
@@ -2,10 +2,15 @@
 
 const welcomeMessage = n.render(
 	`<h1>Hello!</h1>
-	<p>This is a demo of <strong>{{this.name}}</strong>.</p>`,
+	<p>This is a demo of <strong>{{this.name}}</strong>.</p>
+	<button class="testButton">Click me!</button>`,
 	{
 		name: 'No Reaction'
 	}
 );
 
 n.d('body').innerHTML = welcomeMessage;
+
+n.d('.testButton').on('click', (element) => {
+	console.info('You clicked', element.target);
+});
